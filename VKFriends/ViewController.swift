b3436014ca7d2eaad781c5f5d4b04cc1064baaa5
Кирилл Федоров <ikirillfedorov@gmail.com>
2020-03-28 08:35:17 +0300

    Initial Commit

---
+++ VKFriends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKFriends
+//
+//  Created by Kirill Fedorov on 28.03.2020.
+//  Copyright © 2020 Kirill Fedorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
